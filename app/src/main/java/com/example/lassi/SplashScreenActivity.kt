f60128f26b5a61adcfb16b2e8092be288f7e9bae
Manish Pandey <2018140@iiitdmj.ac.in>
2021-04-12 21:52:56 +0530

    Second Commit

---
+++ app/src/main/java/com/example/lassi/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.lassi
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val typeFace : Typeface = Typeface.createFromAsset(assets, "Sacramento-Regular.ttf")
+        tv_app_name.typeface = typeFace
+    }
+}
