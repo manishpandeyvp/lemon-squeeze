a06ef43010432ddb50b70575955ecc577a1801c5
Manish Pandey <2018140@iiitdmj.ac.in>
2021-04-13 23:18:46 +0530

    Fifteenth Commit

---
+++ app/src/main/java/com/example/lassi/activities/JuiceAndShakeRecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lassi.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JuiceAndShakeRecipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juice_and_shake_recipe)
+    }
+}
