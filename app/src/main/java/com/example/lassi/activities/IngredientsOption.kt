7acf200e1760188d59d96215c101a20104217679
Manish Pandey <2018140@iiitdmj.ac.in>
2021-04-15 22:46:19 +0530

    33rd Commit

--- app/src/main/java/com/example/lassi/activities/IngredientsOption.kt
+++ app/src/main/java/com/example/lassi/activities/IngredientsOption.kt
@@ -11,6 +11,8 @@ import com.example.lassi.utils.Constants
 class IngredientsOption : AppCompatActivity() {
 
     private lateinit var mJuiceAndShakesList: ArrayList<Juice>
+    private var mIngredients: ArrayList<String> = ArrayList()
+    private var mFinalIngredients: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +25,21 @@ class IngredientsOption : AppCompatActivity() {
 
         mJuiceAndShakesList = intent.getParcelableArrayListExtra(Constants.JUICE_AND_SHAKES_LIST)!!
         Log.i("mJuiceAndShakesList", mJuiceAndShakesList.toString())
+
+        getIngredientsList()
+        Log.i("mIngredients", mIngredients.toString())
+
+    }
+
+    private fun getIngredientsList(){
+        for (i in mJuiceAndShakesList){
+            Log.i("mIngredientsList", i.ingredients.toString())
+            for (j in i.ingredients){
+                Log.i("Value of j", j)
+                if (!mIngredients.contains(j)){
+                    mIngredients.add(j)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/lassi/adapters/IngredientOptionsListAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.lassi.adapters
+
+import android.content.Context
+import android.content.res.AssetManager
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+open class IngredientOptionsListAdapter(
+    private val context: Context,
+    private val list: ArrayList<String>,
+    private val assets: AssetManager
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
