0ea2d2bf1e753a29bbaa544bbd8f6b84b1f65820
Manish Pandey <2018140@iiitdmj.ac.in>
2021-04-15 22:11:11 +0530

    32nd Commit

--- app/src/main/java/com/example/lassi/activities/IngredientsOption.kt
+++ app/src/main/java/com/example/lassi/activities/IngredientsOption.kt
@@ -2,12 +2,26 @@ package com.example.lassi.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
 import com.example.lassi.R
+import com.example.lassi.models.Juice
+import com.example.lassi.utils.Constants
 
 class IngredientsOption : AppCompatActivity() {
+
+    private lateinit var mJuiceAndShakesList: ArrayList<Juice>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingredients_option)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        mJuiceAndShakesList = intent.getParcelableArrayListExtra(Constants.JUICE_AND_SHAKES_LIST)!!
+        Log.i("mJuiceAndShakesList", mJuiceAndShakesList.toString())
     }
 }
