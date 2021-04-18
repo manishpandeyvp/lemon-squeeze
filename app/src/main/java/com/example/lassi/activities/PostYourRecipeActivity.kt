62eb6612735f071accc009729135443561057f7a
Manish Pandey <2018140@iiitdmj.ac.in>
2021-04-18 18:14:36 +0530

    42nd Commit

---
+++ app/src/main/java/com/example/lassi/activities/PostYourRecipeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lassi.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lassi.R
+
+class PostYourRecipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_your_recipe)
+    }
+}
