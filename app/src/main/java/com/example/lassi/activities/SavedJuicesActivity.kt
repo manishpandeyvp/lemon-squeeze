a941c7206bc9d9fc178b904ead4730d461e2b703
Manish Pandey <2018140@iiitdmj.ac.in>
2021-04-20 13:15:13 +0530

    58th Commit

--- app/src/main/java/com/example/lassi/activities/SavedJuicesActivity.kt
+++ app/src/main/java/com/example/lassi/activities/SavedJuicesActivity.kt
@@ -1,13 +1,32 @@
 package com.example.lassi.activities
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.lassi.R
+import kotlinx.android.synthetic.main.activity_saved_juices.*
 
 class SavedJuicesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saved_juices)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val typeFaceRegular : Typeface = Typeface.createFromAsset(assets, "Quicksand-Regular.ttf")
+        val typeFaceBold : Typeface = Typeface.createFromAsset(assets, "Quicksand-Bold.ttf")
+        val typeFaceSemiBold : Typeface = Typeface.createFromAsset(assets, "Quicksand-SemiBold.ttf")
+        val typeFaceSacramento : Typeface = Typeface.createFromAsset(assets, "Sacramento-Regular.ttf")
+
+        tv_saved_title.typeface = typeFaceBold
+
+        iv_saved_back.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
