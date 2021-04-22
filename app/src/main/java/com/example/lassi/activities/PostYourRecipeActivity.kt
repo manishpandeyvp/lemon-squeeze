2ea0285d09b6206bc85d74c147dcf26ab6875a8a
Manish Pandey <2018140@iiitdmj.ac.in>
2021-04-22 21:53:55 +0530

    69th Commit

--- app/src/main/java/com/example/lassi/activities/PostYourRecipeActivity.kt
+++ app/src/main/java/com/example/lassi/activities/PostYourRecipeActivity.kt
@@ -2,12 +2,15 @@ package com.example.lassi.activities
 
 import android.Manifest
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Typeface
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.view.Window
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.Toast
@@ -15,14 +18,17 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.lassi.R
+import com.example.lassi.models.Juice
 import com.example.lassi.utils.Constants
 import kotlinx.android.synthetic.main.activity_post_your_recipe.*
+import kotlinx.android.synthetic.main.dialog_recipe_title_desc.*
 import kotlinx.android.synthetic.main.item_juice_card.*
 import java.io.IOException
 
 class PostYourRecipeActivity : AppCompatActivity() {
 
     private var mSelectedImageFileUri : Uri? = null
+    private var mJuice: Juice = Juice()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +50,10 @@ class PostYourRecipeActivity : AppCompatActivity() {
             }else{
                 ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), Constants.READ_STORAGE_PERMISSION_CODE)
             }
+        }
+
+        iv_edit_recipe_title_desc.setOnClickListener {
+            showEditTitleDescDialog()
         }
     }
 
@@ -78,5 +88,31 @@ class PostYourRecipeActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    private fun showEditTitleDescDialog(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_recipe_title_desc)
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val title = tv_recipe_title.text.toString()
+        dialog.et_edit_title.setText(title)
+        dialog.et_edit_title.setSelection(title.length)
+        val desc = tv_recipe_desc.text.toString()
+        dialog.et_edit_desc.setText(desc)
+        dialog.et_edit_desc.setSelection(desc.length)
+        dialog.setCanceledOnTouchOutside(false)
+
+        dialog.tv_ok.setOnClickListener {
+            mJuice.title = dialog.et_edit_title.text.toString()
+            mJuice.desc = dialog.et_edit_desc.text.toString()
+            tv_recipe_title.text = mJuice.title
+            tv_recipe_desc.text = mJuice.desc
+            dialog.dismiss()
+        }
+
+        dialog.tv_cancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
